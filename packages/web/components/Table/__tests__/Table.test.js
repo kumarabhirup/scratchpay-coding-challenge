098f0344ba07d6561fc98f2059607773b105e848
Kumar Abhirup <kumar@iqubex.com>
2019-11-26 11:51:32 +0530

    ✅ TEST: Write your first test

---
+++ packages/web/components/Table/__tests__/Table.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import Table from '../index'
+
+describe('Table', () => {
+  it('renders', () => {
+    shallow(<Table />)
+  })
+})
---
+++ packages/web/lib/scssStubForJest.js
@@ -0,0 +1,1 @@
+module.exports = {} // Or other stub data for all SCSS and SASS modules
